14dbb4a99ccec5575cc06b0f5f07455a6f2e2c5d
KylixMedusa <ayush.agarwalmng@gmail.com>
2024-05-19 15:22:49 +0530

    Added a spinner

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.scss";
 
 import React, { useEffect } from "react";
 
+import { Spinner } from "@nextui-org/react";
+
 import API from "./api";
 import { useApp } from "./context";
 import Router from "./Router";
@@ -23,7 +25,15 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <main className="app">{loading ? <div>Loading...</div> : <Router />}</main>
+    <main className="app">
+      {loading ? (
+        <div className="w-full h-full flex justify-center items-center">
+          <Spinner size="lg" />
+        </div>
+      ) : (
+        <Router />
+      )}
+    </main>
   );
 };
 
